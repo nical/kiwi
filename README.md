b12f6b26c73f8a38efb91874a16e8e2e1c40c76c
Nicolas Silva <nical.silva@gmail.com>
2010-11-03 14:57:03 +0100

    first webpage commit

---
+++ README.md
@@ -0,0 +1,67 @@
+<h1> Kiwi </h1>
+
+A generic and easy to use pipeline system. 
+
+
+<img src='http://img808.imageshack.us/img808/4377/kiwiboxespng01.png' border='0'/>
+
+Checkout the [wiki](http://github.com/nical/kiwi/wiki) !
+<h2> Resources and filters </h2>
+
+Kiwi features a powerful pipeline system composed of resources and filters : 
+<ul>
+	<li>A <b>resource</b> (kiwi::core::Resource) is an object that contains input and output ports and give access to the data it contains by providing readers and writers throught these ports.</li>
+	<li>A <b>Filter</b> (kiwi::core::Filter) is a resource that can be executed using its process() method. Filters inherit from resources their connecting system. </li>
+	<li>A <b>Pipeline</b> (kiwi::core::Pipeline) is a filter that contains other filters (including eventually pipelines) and is responsible for processing each of its filters in the correct order. This is the composite design pattern applied to the kiwi system. </li>
+</ul>
+
+<h2> Creating new resources and filters </h2>
+
+Kiwi is designed to make the creation of Resources and Filters very easy. Just create a class deriving from kiwi::core::Resource or kiwi::core::Filter and overload a few methods.
+
+<h3> Resources </h3>
+
+This page is in under construction, more informations are to come soon
+
+<h3> Filters </h3>
+
+This page is in under construction, more informations are to come soon
+
+<h2> The project's advancement </h2>
+
+kiwi 0.2.2
+
+Kiwi is still at an early stage, and interfaces are subject to change.
+
+<h3> Implemented </h3>
+ - Resource base class with all the connection systeme
+ - Filter base class 
+
+<h3> Currently in developement </h3>
+ - The generic ArrayData Resource
+ - A simple string container
+
+<h3> TODO </h3>
+<ul>
+ <li> Pipeline classes. </li>
+ <li> A set of resources and filters to demonstrate kiwi's potential ! </li>
+ <li> A web-based frontend (html+svg+js) to connect graphically kiwi boxes and see the result in live. </li>
+</ul> 
+
+<h2> License </h2>
+
+Most of Kiwi's source code is under the <b>new BSD License</b>. However some of the plugin sets 
+are under other licenses, like the image::cairo plugin set that is under the LGPL. See the
+readme.txt file in each sub-project directory for informations about which license applies.
+
+<h2> Project member(s) </h2>
+
+For know the Kiwi project is designed/developed/maintained by <b>Nicolas Silva</b>.
+
+I would be very happy to share this experience with anyone who is motivated to create and/or test filters and resources based on kiwi!
+
+To contact me:
+
+email: nical.silva@gmail.com <br/>
+twitter: [@nicalsilva](http://twitter.com/#!/nicalsilva)<br/>
+my resume [(fr)](http://github.com/nical/resume/raw/master/fr/NicolasSilva.pdf)<br/>
---
+++ engine/test/generic/ValueContainerTest.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+#ifndef KIWI_VALUECONTAINER_TEST
+#define KIWI_VALUECONTAINER_TEST
+
+#include "core/Commons.hpp"
+
+#include "core/Filter.hpp"
+
+#include "generic/ArrayContainer.hpp"
+#include "generic/ArrayReader.hpp"
+#include "generic/ArrayWriter.hpp"
+#include "generic/ArrayIterator.hpp"
+
+#include "generic/Point.hpp"
+
+#include "utils/types.hpp"
+
+#include <vector>
+
+
+using namespace kiwi;
+using namespace kiwi::core;
+using namespace kiwi::generic;
+
+
+// ---------------------------------------------------------------------
+// --------------------- A simple Filter -------------------------------
+// ---------------------------------------------------------------------
+
+
+
+// compute the sum of two Value<> resources and place it in an other Value<>
+template<typename TValueType>
+class ValueTestFilter : public Filter
+{
+public:
+	enum{ A = 0, B = 1};
+	enum{ WRITE = 0};
+	enum{ RESULT = 0};
+	
+	typedef generic::ValueReader<TValueType> myReader;
+	typedef generic::ValueWriter<TValueType> myWriter;
+// ---------------------------------------------------------------------
+	ValueTestFilter() : Filter()
+	{
+	ScopedBlockMacro(scp_block, "ValueTestFilter::constructor");
+		setLayoutEventEnabled(false);
+		kiwi::string sType( string("value_")+types::str<TValueType>() );
+		addReaderInputPort(sType);
+		addReaderInputPort(sType);
+		
+		addWriterInputPort(sType);	
+		
+		//add a reader output that will be available only when the writer
+		//port is connected
+		addReaderOutputPort(sType);
+		setPortEnabled(readerOutputPort(0),false);
+		setLayoutEventEnabled(true);
+	}
+	~ValueTestFilter() {}
+
+	
+
+// ---------------------------------------------------------------------
+	void process()
+	{
+	ScopedBlockMacro(proc_block, "ValueTestFilter::process()");
+
+		DEBUG_ONLY(		
+			if(!isReady() )
+			{
+				Debug::error() << "ValueTestFilter::Process error : not ready" 
+					<< endl();
+				return;
+			}
+		)//DEBUG_ONLY
+		
+		Debug::print() << "Allocate Reader #0" << endl();
+		myReader A( readerInputPort(0) );
+		
+		Debug::print() << "Allocate Reader #1" << endl();
+		myReader B( readerInputPort(1) );
+		
+		Debug::print() << "Allocate Writer #0" << endl();
+		myWriter result( writerInputPort(0) );
+		
+		Debug::beginBlock( "compute..");
+		
+		result.set( A.get() + B.get() );
+		
+		Debug::endBlock( "compute..");
+		
+		
+		return;
+	}
+	
+	
+// ---------------------------------------------------------------------
+	bool readyCondition()
+	{
+		return (readerInputPort(0).isConnected()
+			&& readerInputPort(1).isConnected()
+			&& writerInputPort(0).isConnected() );
+	}
+	
+	void layoutChanged()
+	{
+		if(writerInputPort(0).isConnected() )
+		{
+			if( !readerOutputPort(0).isEnabled() )
+			{
+				setPortEnabled(readerOutputPort(0),true);
+				ReaderOutputPort& op
+				= writerInputPort(0).connectedOutput()->node()->readerOutputPort(0);
+				bindPort( readerOutputPort(0), op );
+			}
+		}
+		else
+		{
+			readerOutputPort(0).disconnect();
+			setPortEnabled(readerOutputPort(0),false);
+		}
+
+	}
+};
+
+
+
+
+
+
+// ---------------------------------------------------------------------
+// ---------------------------- Test -----------------------------------
+// ---------------------------------------------------------------------
+
+template<typename T>
+void ValueContainerTest()
+{
+ScopedBlockMacro(__scop, "ValueContainerTest")
+	Debug::beginBlock("Allocate the resources");
+		
+		generic::ValueContainer<T> resource1(42);
+		
+		generic::ValueContainer<T> resource2(12);
+		
+		generic::ValueContainer<T> resourceResult(0);
+		
+		ValueTestFilter<T> myTest;
+		ValueTestFilter<T> myTest2;
+		
+	
+		
+		assert( myTest.indexOf( myTest.readerInputPort(0) ) == 0 );
+		assert( resource1.indexOf( resource1.writerOutputPort(0) ) == 0 );
+
+		
+	
+	Debug::endBlock();
+
+	Debug::endl();
+
+	Debug::beginBlock("connect the ports");
+		resource1.readerOutputPort(0) >> myTest.readerInputPort(0);
+		resource2.readerOutputPort(0) >> myTest.readerInputPort(1);
+		resourceResult.writerOutputPort(0) >> myTest.writerInputPort(0);
+		
+		resource1.readerOutputPort(0) >> myTest2.readerInputPort(0);
+		myTest.readerOutputPort(0) >> myTest2.readerInputPort(1);
+		resourceResult.writerOutputPort(0) >> myTest2.writerInputPort(0);
+	Debug::endBlock("connect the ports");
+
+	Debug::print() << endl();
+
+	myTest.process();
+	myTest2.process();
+
+	Debug::print() << "processed" << endl();
+
+	
+}
+
+
+
+#endif

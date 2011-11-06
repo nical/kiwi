e8ec7dcc029ba5153a1b8e3252eb7a597711de34
Nicolas Silva <nical.silva@gmail.com>
2011-11-06 15:42:46 +0100

    [cpp][testing] Implemented mockNode.

--- cpp/src/kiwi/mock/MockNode.cpp
+++ cpp/src/kiwi/mock/MockNode.cpp
@@ -3,20 +3,39 @@
 #include "kiwi/core/Node.hpp"
 #include "kiwi/core/NodeTypeManager.hpp"
 #include "kiwi/core/DataTypeManager.hpp"
+#include "kiwi/mock/MockNodeUpdater.hpp"
+
+using namespace kiwi::core;
 
 namespace kiwi{
 namespace mock{
 
+static const NodeTypeInfo* _mockNodeInfo;
 
+KIWI_DECLARE_CONTAINER(int,"Int");
 
 void DelcareMockNode( uint32 nbInputs, uint32 nbOutputs )
 {
-    KIWI_NOT_IMPLEMENTED("DelcareMockNode");
+    auto IntInfo = DataTypeManager::RegisterDataType("Int", &Newint);
+
+    NodeLayoutDescriptor layout;
+
+    for(int i = 0; i < nbInputs; ++i)
+        layout.inputs.push_back( { "in", IntInfo, READ } );
+
+    for(int i = 0; i < nbInputs; ++i)
+        layout.outputs.push_back( { "out", IntInfo, READ } );
+    
+
+    _mockNodeInfo = NodeTypeManager::RegisterNode(
+        "MockNode", layout, new mock::MockNodeUpdater
+    );
+
 }
 
 core::Node* NewMockNode()
 {
-    KIWI_NOT_IMPLEMENTED("NewMockNode");
+    return _mockNodeInfo->newInstance();
 }
 
 

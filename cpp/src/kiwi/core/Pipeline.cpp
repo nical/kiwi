15b60ebe9c149ccae8337da2fc7e4476d879a529
Nicolas Silva <nical.silva@gmail.com>
2011-11-07 19:50:39 +0100

    [cpp][core] Implemented some of Pipeline's methods.

--- cpp/src/kiwi/core/Pipeline.cpp
+++ cpp/src/kiwi/core/Pipeline.cpp
@@ -21,31 +21,58 @@ Pipeline::~Pipeline()
 
 bool Pipeline::setInput(uint32 index, Data* inputData)
 {
+    KIWI_NOT_IMPLEMENTED("Pipeline::setInput");
     return false;
 }
 
 bool Pipeline::setOutput(uint32 index, Data* inputData)
 {
+    KIWI_NOT_IMPLEMENTED("Pipeline::setOutput");
     return false;
 }
 
 bool Pipeline::addNode( Node* n )
 {
-    return false;
+    if(!contains(n))
+    {
+        _nodes.push_back(n);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static int _findIndexOf( Pipeline::NodeArray nodes, const Node* n )
+{
+    for(int  i=0; i < nodes.size(); ++i)
+        if(nodes[i] == n)
+             return i;
+    return -1;
 }
 
 bool Pipeline::removeNode( Node* n )
 {
+    int idx = _findIndexOf(_nodes, n);
+    if ( idx > 0 )
+    {
+        _nodes[idx] = _nodes[_nodes.size()-1];
+        _nodes.resize(_nodes.size()-1);
+        return true;
+    }
     return false;
 }
 bool Pipeline::removeAllNodes()
 {
-    return false;
+    _nodes.resize(0);
+    return true;
 }
 
 bool Pipeline::contains(const Node* n)
 {
-    return false;
+    int idx = _findIndexOf(_nodes, n);
+    return idx > 0;
 }
 
 }//namespace
--- cpp/src/kiwi/processing/ProcessingPipelineUpdater.cpp
+++ cpp/src/kiwi/processing/ProcessingPipelineUpdater.cpp
@@ -1,5 +1,7 @@
 
 #include "kiwi/processing/ProcessingPipelineUpdater.hpp"
+#include "kiwi/core/Pipeline.hpp"
+#include "kiwi/core/Node.hpp"
 
 using namespace kiwi::core;
 
@@ -9,7 +11,39 @@ namespace processing{
 
 bool ProcessingPipelineUpdater::update( Pipeline* p, uint32 flags )
 {
-    KIWI_NOT_IMPLEMENTED("ProcessingPipelineUpdater::update");
+
+    Pipeline::NodeArray toUpdate = p->nodes();
+    
+    int i = 0;
+    while( toUpdate.size() > 0 )
+    {
+        Node* n = toUpdate[i];
+        auto & dependencies = toUpdate[i]->previousNodes();
+        // look for dependencies 
+        bool foundDependency = false;
+        for(auto it = dependencies.begin(); it!=dependencies.end();++it)
+        {
+            if(n ==*it)
+            {
+                foundDependency = true;
+                break;
+            }
+        }
+        if( !foundDependency )
+        {
+            n->update();
+            // then remove the node from toUpdate
+            toUpdate[i] == toUpdate[ toUpdate.size()-1];
+            toUpdate.resize(toUpdate.size()-1);
+        }
+        else
+        {
+            ++i;
+        }
+        if( i >= toUpdate.size() ) i = 0;
+    }
+    
+    
     return false;
 }
 
